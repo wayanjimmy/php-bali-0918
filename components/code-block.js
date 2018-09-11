5c527c6767da8d25de68200b52be71dbd2dba504
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-09-11 11:34:55 +0800

    remove codemirror

--- components/code-block.js
+++ components/code-block.js
@@ -1,17 +1,39 @@
 import React from 'react'
-import {UnControlled as CodeMirror} from 'react-codemirror2'
+import Highlight, {defaultProps} from 'prism-react-renderer'
+import styled from 'styled-components'
+import theme from 'prism-react-renderer/themes/duotoneLight'
 
-export const CodeBlock = ({children, options = {}, style, ...props}) => (
-  <div style={{fontSize: 18, ...style}}>
-    <CodeMirror
-      value={children}
-      options={{
-        mode: 'php',
-        lineNumbers: false,
-        readOnly: 'nocursor',
-        ...options
-      }}
-      {...props}
-    />
-  </div>
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+
+  & .token-line {
+    line-height: 1em;
+    height: 1em;
+  }
+`
+
+export const LineNo = styled.span`
+  display: inline-block;
+  width: 2em;
+  user-select: none;
+  opacity: 0.3;
+`
+
+export const CodeBlock = ({children, fontSize}) => (
+  <Highlight {...defaultProps} code={children} language="php" theme={theme}>
+    {({className, style, tokens, getLineProps, getTokenProps}) => (
+      <Pre className={className} style={{...style, fontSize}}>
+        {tokens.map((line, i) => (
+          <div {...getLineProps({line, key: i})}>
+            <LineNo>{i + 1}</LineNo>
+            {line.map((token, key) => (
+              <span {...getTokenProps({token, key})} />
+            ))}
+          </div>
+        ))}
+      </Pre>
+    )}
+  </Highlight>
 )
